10cf1df5065b467667e76cca4d3cc6e965817b00
Fabio Sobottka Sarracini <fabio.sarracini-ext@grupocasasbahia.com.br>
2023-10-02 18:06:44 -0300

    [POC] teste unitario camada de applicacao

--- src/test/java/br/com/via/poc/plataformas/app/CreateTransactionServiceTest.java
+++ src/test/java/br/com/via/poc/plataformas/app/CreateTransactionServiceTest.java
@@ -1,11 +1,29 @@
 package br.com.via.poc.plataformas.app;
 
 import br.com.via.poc.plataformas.app.converter.TransactionToEventConverter;
+import br.com.via.poc.plataformas.app.dto.TransactionEvent;
+import br.com.via.poc.plataformas.domain.Transaction;
 import br.com.via.poc.plataformas.domain.Transactions;
+import br.com.via.poc.plataformas.domain.User;
+import br.com.via.poc.plataformas.ui.transaction.dto.TransactionDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CreateTransactionServiceTest {
@@ -19,8 +37,27 @@ class CreateTransactionServiceTest {
     @Mock
     private TransactionToEventConverter converter;
 
-    @Test
-    void shouldCreateTransaction() {
+    private CreateTransactionService subject;
 
+    @BeforeEach
+    public void beforeEach() {
+        subject = new CreateTransactionService(transactions, sender, converter);
     }
+
+    @Test
+    void shouldCreateTransactionAndSendEvent() {
+
+        User user = mock(User.class);
+        TransactionDto transaction = mock(TransactionDto.class);
+        TransactionEvent event = mock(TransactionEvent.class);
+
+        when(converter.convert(any(), any())).thenReturn(event);
+        when(transaction.buildTransaction()).thenReturn(new Transaction("id-qualquer", user, new BigDecimal("200.0"), "Description teste", LocalDateTime.now()));
+
+        subject.execute(transaction);
+
+        verify(sender, times(1)).sendEvent(event);
+        verify(converter, times(1)).convert(any(), any());
+    }
+
 }
