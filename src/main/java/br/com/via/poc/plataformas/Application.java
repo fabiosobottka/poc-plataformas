0ca78fae382ab762c019fc7ab122952d9ed9f326
Fabio Sobottka Sarracini <fabio.sarracini-ext@grupocasasbahia.com.br>
2023-10-02 18:22:49 -0300

    [POC] refactory

--- src/main/java/br/com/via/poc/plataformas/Application.java
+++ src/main/java/br/com/via/poc/plataformas/Application.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class Application {
+class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
